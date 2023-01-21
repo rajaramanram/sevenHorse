8880a3a7cfc11243781fe91ae283b5c4e3f1f315
Rajaraman <61849983+rajaramanram@users.noreply.github.com>
2023-01-21 23:32:18 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const index = () => {
+  return (
+        <>
+        
+    <div>SEVEN HORSES</div>
+    <br/>
+    <a href="/contact">Contact</a>
+    <br/>
+    <a href="/about">About US</a>
+    <br/>
+    <a href="/advertise">Advertise</a>
+    </>
+  )
+}
+
+export default index
